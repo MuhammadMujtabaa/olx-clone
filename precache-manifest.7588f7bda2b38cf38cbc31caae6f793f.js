92dead153c74034c44eac9c90ca9c16022e65d36
MuhammadMujtabaa <51963629+MuhammadMujtabaa@users.noreply.github.com>
2020-11-18 22:47:50 +0500

    olx clone

---
+++ precache-manifest.7588f7bda2b38cf38cbc31caae6f793f.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47c721e4c5b664828a6f03f55c09e1c0",
+    "url": "/olx-clone/index.html"
+  },
+  {
+    "revision": "66b099a4679e02a5d8b4",
+    "url": "/olx-clone/static/css/2.2d63d0b4.chunk.css"
+  },
+  {
+    "revision": "75a7201f157f5a3b7462",
+    "url": "/olx-clone/static/css/main.8db1cc69.chunk.css"
+  },
+  {
+    "revision": "66b099a4679e02a5d8b4",
+    "url": "/olx-clone/static/js/2.52ff24d2.chunk.js"
+  },
+  {
+    "revision": "2df1d758a07e752899350983fc4e9bd6",
+    "url": "/olx-clone/static/js/2.52ff24d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75a7201f157f5a3b7462",
+    "url": "/olx-clone/static/js/main.c9f07180.chunk.js"
+  },
+  {
+    "revision": "6f96a4737af136c966ca",
+    "url": "/olx-clone/static/js/runtime-main.9dd01046.js"
+  },
+  {
+    "revision": "d1349bd849fbebe7efdf8949210a55ee",
+    "url": "/olx-clone/static/media/appstore.d1349bd8.webp"
+  },
+  {
+    "revision": "bdbe75cc9bfd9bafa09917d3011beaca",
+    "url": "/olx-clone/static/media/f1.bdbe75cc.webp"
+  },
+  {
+    "revision": "6f63c367764a68717deeb5d092d17da9",
+    "url": "/olx-clone/static/media/f2.6f63c367.webp"
+  },
+  {
+    "revision": "b6d7991ad65d4e1f883b9fadf4a6eceb",
+    "url": "/olx-clone/static/media/f3.b6d7991a.webp"
+  },
+  {
+    "revision": "b0d72936ca9a5d0fc39552c69a69b90c",
+    "url": "/olx-clone/static/media/f4.b0d72936.webp"
+  },
+  {
+    "revision": "7ab93435b0305f87368f8f4c44d51787",
+    "url": "/olx-clone/static/media/hero.7ab93435.PNG"
+  },
+  {
+    "revision": "28cd6fa2888f1b2bfd60de578ce4b373",
+    "url": "/olx-clone/static/media/olx-logo.28cd6fa2.png"
+  },
+  {
+    "revision": "882972cbafc28c2e00afca2f69d7b9f5",
+    "url": "/olx-clone/static/media/phone-app.882972cb.webp"
+  },
+  {
+    "revision": "959f19ef1afe0ce337a8998709a1ae76",
+    "url": "/olx-clone/static/media/playstore.959f19ef.webp"
+  },
+  {
+    "revision": "60a14964a1b1a6251259946efaa91eb4",
+    "url": "/olx-clone/static/media/r1.60a14964.webp"
+  },
+  {
+    "revision": "e09604bf63e883449e6897c7d13e71b0",
+    "url": "/olx-clone/static/media/r10.e09604bf.webp"
+  },
+  {
+    "revision": "5ec873c47f41b12423a7453591953765",
+    "url": "/olx-clone/static/media/r11.5ec873c4.webp"
+  },
+  {
+    "revision": "ce2e5624ae307fa3c1253de9f463d939",
+    "url": "/olx-clone/static/media/r12.ce2e5624.webp"
+  },
+  {
+    "revision": "3ca47bc1c6d1f170a8f976a5b31b88db",
+    "url": "/olx-clone/static/media/r2.3ca47bc1.webp"
+  },
+  {
+    "revision": "def41a387bb8c9733c81e165c3030873",
+    "url": "/olx-clone/static/media/r3.def41a38.webp"
+  },
+  {
+    "revision": "9d297803675e15f8724cd0efe9adb42f",
+    "url": "/olx-clone/static/media/r4.9d297803.webp"
+  },
+  {
+    "revision": "d482dd5fde133975072573bf1b5f55d8",
+    "url": "/olx-clone/static/media/r5.d482dd5f.webp"
+  },
+  {
+    "revision": "4146b510821f60c5a2a98cc52d182c7c",
+    "url": "/olx-clone/static/media/r6.4146b510.webp"
+  },
+  {
+    "revision": "2b855612013b961e05dae56d7d4a2435",
+    "url": "/olx-clone/static/media/r7.2b855612.webp"
+  },
+  {
+    "revision": "51505c347604efa06bf711a64a642d79",
+    "url": "/olx-clone/static/media/r8.51505c34.webp"
+  },
+  {
+    "revision": "8904ce8f966f8b581b79dc9695ece7f6",
+    "url": "/olx-clone/static/media/r9.8904ce8f.webp"
+  },
+  {
+    "revision": "96e9e5e9ef2344ded9755085347895f8",
+    "url": "/olx-clone/static/media/search-icon.96e9e5e9.png"
+  }
+]);
